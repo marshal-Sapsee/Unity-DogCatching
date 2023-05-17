0b11b8b09ecd7d9ddc909d0fd770abd98e5a204f
sapsee <mail.sapsee@gmail.com>
2023-05-17 17:01:23 +0300

    Initialize on git

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float lastTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Time.realtimeSinceStartup - lastTime > 3f)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                lastTime = Time.realtimeSinceStartup;
+            }
+        }
+    }
+}
